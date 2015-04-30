8fbb33828aac7861c8e6e5707736d6427357364a
Douglas Andreani <andreani.dr@gmail.com>
2015-04-29 21:56:53 -0300

    pylint standards

--- user_socket.py
+++ user_socket.py
@@ -1,62 +1,53 @@
 import socket
 
-class user_socket:
-    _ip='127.0.0.1'
-    _port=32001
-    _socket=None
-    _buffer=1024
-    _lastReceivedMessage=None
+class UserSocket:
+    def __init__(self, ip_in='127.0.0.1', port_in='32001'):
+        self._ip = ip_in
+        self._port = port_in
 
-    _outIp=None
-    _outPort=None
+    _socket = None
+    _buffer = 1024
+    _lastReceivedMessage = None
+    _out_ip = None
+    _out_port = None
     def initialize_socket(self):
-       self._socket = socket.socket(socket.AF_INET, # Internet
+        self._socket = socket.socket(socket.AF_INET, # Internet
                             socket.SOCK_DGRAM) # UDP
-    def setIp(self, ip):
-        self._ip=ip
-    def getIp(self):
+    def set_ip(self, ip_in):
+        self._ip = ip_in
+    def get_ip(self):
         return self._ip
-    def setPort(self, port):
-        self._port=port
-    def getPort(self):
+    def set_port(self, port_in):
+        self._port = port_in
+    def get_port(self):
         return self._port
-    def getLastReceivedMessage(self):
+    def get_last_received_message(self):
         return self._lastReceivedMessage
-    def getOutIp(self):
-        return self._outIp
-    def getOutPort(self):
-        return self._outPort
+    def get_out_ip(self):
+        return self._out_ip
+    def get_out_port(self):
+        return self._out_port
 
 
-    def isInitialized(self):
+    def is_initialized(self):
         if (self._socket):
             return True
         else:
             return False
-    def sendMessage(self, message):
+    def send_message(self, message):
         self._socket.sendto(bytes(message, 'UTF-8'), (self._ip, self._port))
-    def receiveMessage(self):
+    def receive_message(self):
         message_struct = self._socket.recvfrom(self._buffer)
-        self._outIp=message_struct[1][0]
-        self._outPort=message_struct[1][1]
+        self._out_ip = message_struct[1][0]
+        self._out_port = message_struct[1][1]
         #self._lastReceivedMessage = 
         return message_struct
 
-class client(user_socket):
+class Client(UserSocket):
     def __init__(self, client_name):
-        self.client_name=client_name
+        self.client_name = client_name
 
 
-class server(user_socket):
+class Server(UserSocket):
     def __init__(self, server_name):
-        self.server_name=server_name
-
-if __name__ == "__main__":
-    new_client = client("cliente 1")
-    if not (new_client.isInitialized()):
-        new_client.initialize_socket()
-    print(new_client.getIp())
-    new_client.sendMessage("Ola Mundo")
-    new_client.receiveMessage();
-    #print(new_client.getLastReceivedMessage())
-    print(new_client.getOutIp())
+        self.server_name = server_name
